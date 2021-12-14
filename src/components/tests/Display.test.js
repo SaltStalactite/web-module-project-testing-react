274d401aa51467222ac95566b04746593eef18e9
cc <votaryofturms@icloud.com>
2021-12-14 01:28:26 -0500

    renders display without props

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen, waitFor} from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
 
+const testShow = {
+    name: 'Filler show name',
+    summary: 'Filler show summary',
+    seasons: [
+        {
+            id: 0,
+            name: 'Season 1',
+            episodes: []
+        },
+        {
+            id: 1,
+            name: 'Season 2',
+            episodes: []
+        },
+        {
+            id: 2,
+            name: 'Season 3',
+            episodes: []
+        }
+    ]
+}
 
-test('renders without errors with no props', ()=>{
+jest.mock('./../../api/fetchShow')
 
+test('renders without errors with no props', () => {
+    render(<Display />)
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders Show component when the button is clicked ', () => { });
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+test('renders show season options matching your data when the button is clicked', () => { });
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+test('renders show season options matching your data when the button is clicked', () => { });
