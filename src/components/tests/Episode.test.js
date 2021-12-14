9a3984679d8164d7d51e80027728be30f5251884
cc <votaryofturms@icloud.com>
2021-12-13 23:52:31 -0500

    episode renders without error test

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,9 +3,20 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
+const testEpisode = {
+    id: 1,
+    name: "",
+    image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+    season: 1,
+    number: 1,
+    summary: "This is sample summary text",
+    runtime: 1
+}
 
-test("renders without error", () => {});
+test("renders without error", () => {
+    render(<Episode episode={testEpisode} />)
+});
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders the summary test passed as prop", () => { });
 
-test("renders default image when image is not defined", ()=>{});
+test("renders default image when image is not defined", () => { });
