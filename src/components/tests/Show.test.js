a317e920639138b854b0bc54427c5bdf45c4dacd
cc <votaryofturms@icloud.com>
2021-12-14 00:12:38 -0500

    test data for show tests

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -5,13 +5,35 @@ import userEvent from '@testing-library/user-event';
 
 import Show from './../Show';
 
-test('renders without errors', ()=>{});
+const testShow = {
+    name: 'Filler show name',
+    summary: 'Filler show summary',
+    seasons: [
+        {
+            id: 0,
+            name: 'Season 1',
+            episodes: []
+        },
+        {
+            id: 1,
+            name: 'Season 2',
+            episodes: []
+        },
+        {
+            id: 2,
+            name: 'Season 3',
+            episodes: []
+        }
+    ]
+}
 
-test('renders Loading component when prop show is null', () => {});
+test('renders without errors', () => { });
+
+test('renders Loading component when prop show is null', () => { });
 
 
-test('renders same number of options seasons are passed in', ()=>{});
+test('renders same number of options seasons are passed in', () => { });
 
-test('handleSelect is called when an season is selected', () => {});
+test('handleSelect is called when an season is selected', () => { });
 
-test('component renders when no seasons are selected and when rerenders with a season passed in', () => {});
+test('component renders when no seasons are selected and when rerenders with a season passed in', () => { });
