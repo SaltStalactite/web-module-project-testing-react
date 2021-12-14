d468ed225ba22208eb777d72869f85f4f172877e
cc <votaryofturms@icloud.com>
2021-12-14 01:55:14 -0500

    shows component when button is pressed

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
+import mockFetchShow from './../../api/fetchShow'
 import Display from './../Display';
 
 const testShow = {
@@ -33,7 +33,19 @@ test('renders without errors with no props', () => {
     render(<Display />)
 });
 
-test('renders Show component when the button is clicked ', () => { });
+test('renders Show component when the button is clicked ', async () => {
+    mockFetchShow.mockResolvedValueOnce(testShow)
+
+    render(<Display />)
+
+    const button = screen.getByRole('button')
+
+    userEvent.click(button)
+
+    const show = await screen.findByTestId('show-container')
+
+    expect(show).toBeInTheDocument()
+});
 
 test('renders show season options matching your data when the button is clicked', () => { });
 
